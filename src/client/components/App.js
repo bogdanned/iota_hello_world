31aada3da8a75f46c778e29cd56702e81eda095e
Bogdan Nedelcu <bogdan.nedelcu@lokalleads.de>
2017-08-26 18:44:08 +0200

    src

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -1,5 +1,10 @@
 import React from 'react'
 
 export default () => (
-  <h1>App IOTA</h1>
+  <div>
+    <h1>App IOTA</h1>
+    <h3>FQCDBIETYUYAEPNVLIVGC9XWZWWTSKHQNAMK9BSHETDYOWEFPCDYGCAUNDREAIOHWEM9XLTXDMTYOJRHWQSQXIAXAZ</h3>
+    <h3>Balance:</h3>
+    <h3></h3>
+  </div>
 )
--- src/server/setup.js
+++ src/server/setup.js
@@ -19,6 +19,37 @@ export default (app) => {
         }
     })
   })
+
+
+  app.get('/iota', (req, res) => res.send(200, 'IOTA Version: ' + iota.version ))
+
+
+  app.get('/iota/getBalances', (req, res) => {
+    const seed = 'FQCDBIETYUYAEPNVLIVGC9XWZWWTSKHQNAMK9BSHETDYOWEFPCDYGCAUNDREAIOHWEM9XLTXDMTYOJRHWQSQXIAXAZ'
+    iota.api.getAccountData(seed, (error, success) => {
+        if (error) {
+          res.status(200).send('Error: ' + error )
+        } else {
+          res.status(200).send('Node Info: ' + JSON.stringify(success))
+        }
+      })
+    }
+  )
+
+
+  app.get('/iota/attachToTangle', (req, res) => {
+    const seed = 'FQCDBIETYUYAEPNVLIVGC9XWZWWTSKHQNAMK9BSHETDYOWEFPCDYGCAUNDREAIOHWEM9XLTXDMTYOJRHWQSQXIAXAZ'
+    iota.api.getAccountData(seed, (error, success) => {
+        if (error) {
+          res.status(200).send('Error: ' + error )
+        } else {
+          res.status(200).send('Node Info: ' + JSON.stringify(success))
+        }
+      })
+    }
+  )
+
+
   app.get('/test', (req, res) => res.statues(200).send('IOTA Test: ' ))
 
 }
