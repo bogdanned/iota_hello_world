18dcb72203c73762a0bf39180b0591180ab8e78e
Bogdan Nedelcu <bogdan.nedelcu@lokalleads.de>
2017-08-26 15:46:46 +0200

    server

--- server/index.js
+++ server/index.js
@@ -8,15 +8,26 @@ const app = express()
 app.get('/', (req, res) => res.sendFile( path.resolve(__dirname, '../client/index.html')))
 
 
-
-
 const iota = new IOTA({
     'host': 'http://localhost',
     'port': 14265
-});
+})
 
 
 app.get('/iota', (req, res) => res.send(200, 'IOTA Version: ' + iota.version ))
+
+app.get('/iota/node-info', (req, res) => {
+  iota.api.getNodeInfo(function(error, success) {
+      if (error) {
+          console.error(error);
+      } else {
+          console.log(success);
+      }
+  })
+  res.send(200, 'Node Info: ' + iota.version )
+})
+
+
 
 app.get('/test', (req, res) => res.send(200, 'IOTA Test: ' ))
 
