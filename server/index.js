a4ff35d2d4787d2bbe0ecceb09a5b19f5b337cdf
Bogdan Nedelcu <bogdan.nedelcu@lokalleads.de>
2017-08-26 15:21:59 +0200

    package

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import express from "express"
 const app = express()
 
 
-app.get('/*', (req, res) => res.sendfile('./server/index.html'))
+app.get('/*', (req, res) => res.sendfile('./client/index.html'))
 
 
 app.listen(3000, () =>(console.log('IOTA app listening on port 3000!')))
